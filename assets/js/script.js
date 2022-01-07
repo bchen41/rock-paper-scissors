dc7eb402a6df80d6d4dd5c625de8f849e6316909
bchen41 <bettychen41@outlook.com>
2022-01-07 00:49:37 -0500

    created function playRound: case insensitive for player's input and returns string declaring winner of round

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,31 @@ function computerPlay() {
 function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+function playRound(playerSelection, computerSelection) {
+  const playerSelectionLowerCase = playerSelection.toLowerCase();
+  if (playerSelectionLowerCase === computerSelection) {
+    return `You tied!`;
+  }
+  if (playerSelectionLowerCase === "rock") {
+    if (computerSelection === "paper") {
+      return `You lose! Paper beats rock!`;
+    } else {
+      return `You win! Rock beats scissors!`;
+    }
+  }
+  if (playerSelectionLowerCase === "paper") {
+    if (computerSelection === "scissors") {
+      return `You lose! Scissors beat paper!`;
+    } else {
+      return `You win! Paper beats rock!`;
+    }
+  }
+  if (playerSelectionLowerCase === "scissors") {
+    if (computerSelection === "rock") {
+      return `You lose! Rock beats scissors!`;
+    } else {
+      return `You win! Scissors beat paper!`;
+    }
+  }
+}
